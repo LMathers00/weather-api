a22c3897c630567a6f10e11c3ec918bb4f247cdd
LMathers00 <lukemathers@hotmail.co.uk>
2022-09-27 16:24:36 +0100

    added links to maps both basic and directions pt1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
+
 function App() {
     const [userCoordinates, setUserCoordinates] = useState();
     const [weatherData, setWeatherData] = useState();
@@ -100,15 +101,10 @@ function App() {
             <p>Want to see the news near you? <a href={`https://www.bbc.co.uk/search?q=${weatherData?.location.region}`} target="_blank"> CLICK HERE!</a></p>
             <p>Or search news by region by... <a href={`https://www.bbc.co.uk/news/localnews`} target="_blank"> clicking here</a></p>
             <p>NEED TO TRY AND GET GOOGLE API MAPS WORKING ETC</p>
-            <iframe
-        title="google-maps"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3172.3325395304414!2d-122.01116148467422!3d37.33463524513264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb59127ce078f%3A0x18e1c3ce7becf1b!2sApple%20Park!5e0!3m2!1sen!2sin!4v1637309850935!5m2!1sen!2sin"
-        width="600"
-        height="450"
-        style={{ border: "0" }}
-        allowfullscreen=""
-        loading="lazy"
-      ></iframe>
+            <p>Take a look at your local area <a href={`https://www.google.co.uk/maps/@${userCoordinates},12z`} target="_blank"> here</a></p>
+
+            <p>Need directions? Just tell me <a href={`https://www.google.co.uk/maps/dir/${userCoordinates}/blackpool`} target="_blank"> where to??</a></p>
+            
 
             
         </div>
